ae89ac96ae9f23cb63d32e9cc32db492b21c9733
anirudh <anirudhkolgaonkar@gmail.com>
2020-10-14 12:59:22 +0530

    created module for error debug

--- app/src/main/java/com/anirudh/logutilexample/MainActivity.kt
+++ app/src/main/java/com/anirudh/logutilexample/MainActivity.kt
@@ -2,10 +2,13 @@ package com.anirudh.logutilexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.anirudh.logutil.LogDebug
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        LogDebug.d("My custom log")
     }
 }
---
+++ errorutil/src/main/java/com/anirudh/errorutil/ErrorDebug.kt
@@ -0,0 +1,12 @@
+package com.anirudh.errorutil
+
+import android.util.Log
+
+class ErrorDebug {
+    companion object {
+        val TAG = "SUPER_AWESOME_APP"
+        fun e(message:String) {
+            Log.e(TAG,message)
+        }
+    }
+}
